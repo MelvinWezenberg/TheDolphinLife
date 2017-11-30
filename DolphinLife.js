b5e7a9e98d75e2945d6e95120d24a36ea97e5c15
JOttmann <33494120+JOttmann@users.noreply.github.com>
2017-11-30 23:50:41 +0100

    css

--- DolphinLife.js
+++ DolphinLife.js
@@ -48,8 +48,14 @@ $(document).ready(()=> {
 
 // formu
 
-$("#newsletter").click(function(){
-  alert("You have successfully signed up for the Newsletter! Thank you for being a Dolphiner :)");
-});
 
+function validate()
+   {
+
+      if( document.First Name.value == "" )
+      {
+         alert( "Please provide your First Name!" );
+         document.First Name.focus() ;
+         return false;
+      };
 });
